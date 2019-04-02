2ac36aee7c8eb7404275e38b6b66445672156690
Kieron Browne <kbrowne@pivotal.io>
2019-04-02 22:06:16 +0100

    implement some tuple arithmetic

--- geometry/tuple.go
+++ geometry/tuple.go
@@ -1,5 +1,7 @@
 package geometry
 
+import "math"
+
 const EPSILON = 0.00001
 
 type Tuple struct {
@@ -7,6 +9,10 @@ type Tuple struct {
 	Y float64
 	Z float64
 	W float64
+}
+
+func NewTuple(x, y, z, w float64) Tuple {
+	return Tuple{X: x, Y: y, Z: z, W: w}
 }
 
 func Point(x, y, z float64) Tuple {
@@ -30,6 +36,55 @@ func (t Tuple) Equals(s Tuple) bool {
 		floatEquals(t.X, s.X) &&
 		floatEquals(t.Z, s.Z) &&
 		floatEquals(t.W, s.W)
+}
+
+func (t Tuple) Add(s Tuple) Tuple {
+	return Tuple{
+		X: t.X + s.X,
+		Y: t.Y + s.Y,
+		Z: t.Z + s.Z,
+		W: t.W + s.W,
+	}
+}
+
+func (t Tuple) Subtract(s Tuple) Tuple {
+	return Tuple{
+		X: t.X - s.X,
+		Y: t.Y - s.Y,
+		Z: t.Z - s.Z,
+		W: t.W - s.W,
+	}
+}
+
+func (t Tuple) Negate() Tuple {
+	return Tuple{
+		X: -t.X,
+		Y: -t.Y,
+		Z: -t.Z,
+		W: -t.W,
+	}
+}
+
+func (t Tuple) Multiply(c float64) Tuple {
+	return Tuple{
+		X: t.X * c,
+		Y: t.Y * c,
+		Z: t.Z * c,
+		W: t.W * c,
+	}
+}
+
+func (t Tuple) Divide(c float64) Tuple {
+	return Tuple{
+		X: t.X / c,
+		Y: t.Y / c,
+		Z: t.Z / c,
+		W: t.W / c,
+	}
+}
+
+func (t Tuple) Magnitude() float64 {
+	return math.Sqrt(t.X*t.X + t.Y*t.Y + t.Z*t.Z)
 }
 
 func floatEquals(f, g float64) bool {
--- geometry/tuple_test.go
+++ geometry/tuple_test.go
@@ -1,6 +1,8 @@
 package geometry_test
 
 import (
+	"math"
+
 	"github.com/kieron-pivotal/rays/geometry"
 	"github.com/kieron-pivotal/rays/geometry/tuple_matcher"
 	. "github.com/onsi/ginkgo"
@@ -11,7 +13,7 @@ var _ = Describe("Tuple", func() {
 
 	Context("point", func() {
 		It("is a point when w is 1.0", func() {
-			t := geometry.Tuple{X: 4.3, Y: -4.2, Z: 3.1, W: 1.0}
+			t := geometry.NewTuple(4.3, -4.2, 3.1, 1.0)
 			Expect(t.X).To(Equal(4.3))
 			Expect(t.Y).To(Equal(-4.2))
 			Expect(t.Z).To(Equal(3.1))
@@ -28,7 +30,7 @@ var _ = Describe("Tuple", func() {
 
 	Context("vector", func() {
 		It("is a vector when w is 0.0", func() {
-			t := geometry.Tuple{X: 4.3, Y: -4.2, Z: 3.1, W: 0.0}
+			t := geometry.NewTuple(4.3, -4.2, 3.1, 0.0)
 			Expect(t.X).To(Equal(4.3))
 			Expect(t.Y).To(Equal(-4.2))
 			Expect(t.Z).To(Equal(3.1))
@@ -71,4 +73,99 @@ var _ = Describe("Tuple", func() {
 		})
 	})
 
+	Context("arithmetic", func() {
+		It("can add points and vectors", func() {
+			p := geometry.Point(3, -2, 5)
+			v := geometry.Vector(-2, 3, 1)
+			Expect(p.Add(v)).To(tuple_matcher.Equal(geometry.Point(1, 1, 6)))
+		})
+
+		It("can add two vectors", func() {
+			v := geometry.Vector(3, -2, 1)
+			w := geometry.Vector(1, -5, -4)
+			Expect(v.Add(w)).To(tuple_matcher.Equal(geometry.Vector(4, -7, -3)))
+		})
+
+		It("produces nonsense when adding two points", func() {
+			p := geometry.Point(1, 2, 3)
+			q := geometry.Point(4, 5, 6)
+			r := p.Add(q)
+			Expect(r.IsPoint()).To(BeFalse())
+			Expect(r.IsVector()).To(BeFalse())
+		})
+
+		It("can subtract two points to give a vector", func() {
+			p := geometry.Point(1, 2, 3)
+			q := geometry.Point(4, 5, 6)
+			v := p.Subtract(q)
+			Expect(v).To(tuple_matcher.Equal(geometry.Vector(-3, -3, -3)))
+		})
+
+		It("can subtract two vectors to give another vector", func() {
+			v := geometry.Vector(3, 1, 2)
+			w := geometry.Vector(1, 2, 1)
+			u := v.Subtract(w)
+			Expect(u).To(tuple_matcher.Equal(geometry.Vector(2, -1, 1)))
+		})
+
+		It("can subtract a vector from a point to give a point", func() {
+			p := geometry.Point(3, 2, 1)
+			v := geometry.Vector(5, 6, 7)
+			Expect(p.Subtract(v)).To(tuple_matcher.Equal(geometry.Point(-2, -4, -6)))
+		})
+
+		It("can subtract a vector from a vector to give a vector", func() {
+			v := geometry.Vector(3, 2, 1)
+			w := geometry.Vector(5, 6, 7)
+			Expect(v.Subtract(w)).To(tuple_matcher.Equal(geometry.Vector(-2, -4, -6)))
+		})
+	})
+
+	Context("negating", func() {
+		It("can negate an arbitrary tuple", func() {
+			t := geometry.NewTuple(1, -2, 3, -4)
+			Expect(t.Negate()).To(tuple_matcher.Equal(geometry.NewTuple(-1, 2, -3, 4)))
+		})
+
+		It("can negate a vector", func() {
+			v := geometry.Vector(1, -2, 3)
+			Expect(v.Negate()).To(tuple_matcher.Equal(geometry.Vector(-1, 2, -3)))
+		})
+	})
+
+	Context("scalar multiplication", func() {
+		It("can multiple a tuple by a scalar", func() {
+			v := geometry.NewTuple(1, -2, 3, -4)
+			Expect(v.Multiply(3.5)).To(tuple_matcher.Equal(geometry.NewTuple(3.5, -7, 10.5, -14)))
+		})
+
+		It("can multiple a tuple by a fractional scalar", func() {
+			v := geometry.NewTuple(1, -2, 3, -4)
+			Expect(v.Multiply(0.5)).To(tuple_matcher.Equal(geometry.NewTuple(0.5, -1, 1.5, -2)))
+		})
+	})
+
+	Context("scalar division", func() {
+		It("can divide a tuple by a scalar", func() {
+			t := geometry.NewTuple(1, -2, 3, -4)
+			Expect(t.Divide(2)).To(tuple_matcher.Equal(geometry.NewTuple(0.5, -1, 1.5, -2)))
+		})
+	})
+
+	Context("magnitude", func() {
+		It("can give the magnitude of a vector", func() {
+			x := geometry.Vector(1, 0, 0)
+			Expect(x.Magnitude()).To(BeNumerically("~", 1))
+			y := geometry.Vector(0, 1, 0)
+			Expect(y.Magnitude()).To(BeNumerically("~", 1))
+			z := geometry.Vector(0, 0, 1)
+			Expect(z.Magnitude()).To(BeNumerically("~", 1))
+
+			v := geometry.Vector(1, 2, 3)
+			Expect(v.Magnitude()).To(BeNumerically("~", math.Sqrt(14)))
+
+			w := geometry.Vector(-1, -2, -3)
+			Expect(w.Magnitude()).To(BeNumerically("~", math.Sqrt(14)))
+		})
+	})
 })

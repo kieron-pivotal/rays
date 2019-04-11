fe8676b13dda332d0dd3d5c9e59424996c3db620
Kieron Browne <kbrowne@pivotal.io>
2019-04-11 21:03:34 +0100

    allow playing with dot count

--- play/clock_test.go
+++ play/clock_test.go
@@ -17,16 +17,18 @@ var _ = Describe("Clock", func() {
 	It("can draw a clockface", func() {
 
 		c := canvas.New(600, 600)
+		count := 12
+		col := color.New(255, 255, 0)
 
-		for i := 0; i < 12; i++ {
+		for i := 0; i < count; i++ {
 			p := tuple.Point(1, 0, 0)
 			t := matrix.Identity(4, 4).
-				RotationZ(float64(i)*math.Pi/6).
+				RotationZ(float64(i)*2*math.Pi/float64(count)).
 				Scaling(250, 250, 250).
 				Translation(300, 300, 0)
 
 			p = t.TupleMultiply(p)
-			c.SetPixel(int(math.Round(p.X)), int(math.Round(p.Y)), color.New(255, 255, 255))
+			c.SetPixel(int(math.Round(p.X)), int(math.Round(p.Y)), col)
 		}
 
 		file, err := os.Create("clock.ppm")

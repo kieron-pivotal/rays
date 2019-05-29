e13cb65e32aed2e7d14a9894a715c1e061e7dddb
Kieron Browne <kbrowne@pivotal.io>
2019-05-29 09:15:17 +0100

    make local intersect and normal work for unit cube

--- shape/cube.go
+++ shape/cube.go
@@ -1,6 +1,8 @@
 package shape
 
 import (
+	"math"
+
 	"github.com/kieron-pivotal/rays/ray"
 	"github.com/kieron-pivotal/rays/tuple"
 )
@@ -12,10 +14,45 @@ func (c Cube) Name() string {
 	return "Cube"
 }
 
-func (c Cube) LocalIntersect(ray.Ray) []float64 {
-	return []float64{0}
+func (c Cube) LocalIntersect(r ray.Ray) []float64 {
+	xtmin, xtmax := c.checkAxis(r.Origin.X, r.Direction.X)
+	ytmin, ytmax := c.checkAxis(r.Origin.Y, r.Direction.Y)
+	ztmin, ztmax := c.checkAxis(r.Origin.Z, r.Direction.Z)
+	tmin := math.Max(xtmin, math.Max(ytmin, ztmin))
+	tmax := math.Min(xtmax, math.Min(ytmax, ztmax))
+
+	if tmin > tmax {
+		return []float64{}
+	}
+	return []float64{tmin, tmax}
 }
 
-func (c Cube) LocalNormalAt(tuple.Tuple) tuple.Tuple {
-	return tuple.Vector(0, 0, 0)
+func (c Cube) checkAxis(origin, direction float64) (tmin, tmax float64) {
+	inf := 1e100
+	tmin_numerator := -1 - origin
+	tmax_numerator := 1 - origin
+
+	if math.Abs(direction) > tuple.EPSILON {
+		tmin = tmin_numerator / direction
+		tmax = tmax_numerator / direction
+	} else {
+		tmin = tmin_numerator * inf
+		tmax = tmax_numerator * inf
+	}
+
+	if tmin > tmax {
+		tmin, tmax = tmax, tmin
+	}
+	return
+}
+
+func (c Cube) LocalNormalAt(p tuple.Tuple) tuple.Tuple {
+	maxc := math.Max(math.Abs(p.X), math.Max(math.Abs(p.Y), math.Abs(p.Z)))
+	if maxc == math.Abs(p.X) {
+		return tuple.Vector(p.X, 0, 0)
+	}
+	if maxc == math.Abs(p.Y) {
+		return tuple.Vector(0, p.Y, 0)
+	}
+	return tuple.Vector(0, 0, p.Z)
 }
--- shape/cube_test.go
+++ shape/cube_test.go
@@ -13,7 +13,7 @@ var _ = Describe("Cube", func() {
 
 	Context("ray intersection", func() {
 
-		DescribeTable("t values", func(rayOrigin, rayDirection tuple.Tuple, t1, t2 float64) {
+		DescribeTable("t values when ray intersects cube", func(rayOrigin, rayDirection tuple.Tuple, t1, t2 float64) {
 			c := shape.Cube{}
 			r := ray.New(rayOrigin, rayDirection)
 			xs := c.LocalIntersect(r)
@@ -31,6 +31,38 @@ var _ = Describe("Cube", func() {
 			Entry("inside", tuple.Point(0, 0.5, 0), tuple.Vector(0, 0, 1), -1.0, 1.0),
 		)
 
+		DescribeTable("ray misses cube", func(rayOrigin, rayDirection tuple.Tuple) {
+			c := shape.Cube{}
+			r := ray.New(rayOrigin, rayDirection)
+			xs := c.LocalIntersect(r)
+			Expect(len(xs)).To(Equal(0))
+		},
+
+			Entry("1", tuple.Point(-2, 0, 0), tuple.Vector(0.2673, 0.5345, 0.8018)),
+			Entry("2", tuple.Point(0, -2, 0), tuple.Vector(0.8018, 0.2673, 0.5345)),
+			Entry("3", tuple.Point(0, 0, -2), tuple.Vector(0.5345, 0.8018, 0.2673)),
+			Entry("4", tuple.Point(2, 0, 2), tuple.Vector(0, 0, -1)),
+			Entry("5", tuple.Point(0, 2, 2), tuple.Vector(0, -1, 0)),
+			Entry("6", tuple.Point(2, 2, 0), tuple.Vector(-1, 0, 0)),
+		)
+
+	})
+
+	Context("normals", func() {
+		DescribeTable("normals on cube surfaces", func(p, normal tuple.Tuple) {
+			c := shape.Cube{}
+			Expect(c.LocalNormalAt(p)).To(tuple.Equal(normal))
+		},
+
+			Entry("1", tuple.Point(1, 0.5, -0.8), tuple.Vector(1, 0, 0)),
+			Entry("2", tuple.Point(-1, -0.2, 0.9), tuple.Vector(-1, 0, 0)),
+			Entry("3", tuple.Point(-0.4, 1, -0.1), tuple.Vector(0, 1, 0)),
+			Entry("4", tuple.Point(0.3, -1, -0.7), tuple.Vector(0, -1, 0)),
+			Entry("5", tuple.Point(-0.6, 0.3, 1), tuple.Vector(0, 0, 1)),
+			Entry("6", tuple.Point(0.4, 0.4, -1), tuple.Vector(0, 0, -1)),
+			Entry("7", tuple.Point(1, 1, 1), tuple.Vector(1, 0, 0)),
+			Entry("8", tuple.Point(-1, -1, -1), tuple.Vector(-1, 0, 0)),
+		)
 	})
 
 })
